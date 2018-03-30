6b327aadd28ff7b121629c09a9cd95e99115bf4c
TomaszMoroz <tmoroz688@gmail.com>
2018-03-30 07:45:05 +0200

    bootstrap-vue

--- gallery/src/main.js
+++ gallery/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource'
+import BootstrapVue from 'bootstrap-vue'
 
 Vue.use(VueResource);
+Vue.use(BootstrapVue);
 
 
 new Vue({
